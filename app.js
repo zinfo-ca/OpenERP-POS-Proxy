d55a70de282bc54591a90d9ea2f5a8c6d1b2b3f3
Jean-Philippe Blais <jphblais@zinfo.ca>
2013-05-15 11:54:19 -0400

    Listen only on localhost:port if not in development environment.

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ app.set('port', process.env.PORT || 8069);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.set('jsonp callback', true);
+app.set('env', 'development');
+
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
@@ -32,6 +34,12 @@ app.post('/pos/print_receipt', pos.print_receipt);
 app.get('/pos/open_cashbox', pos.open_cashbox);
 app.get('/pos/pole_display', pos.pole_display);
 
-http.createServer(app).listen(app.get('port'), function() {
-    console.log('OpenERP POS proxy server listening on port ' + app.get('port'));
-});
+if ('development' == app.get('env')) {
+    http.createServer(app).listen(app.get('port'), function() {
+        console.log('OpenERP POS proxy server listening on port *:' + app.get('port'));
+    });
+} else {
+    http.createServer(app).listen(app.get('port'), 'localhost', function() {
+        console.log('OpenERP POS proxy server listening on port localhost:' + app.get('port'));
+    });
+}
